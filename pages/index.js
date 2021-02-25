8e38f76a63175037e5db97609ab3b1859de1c0fd
xaisoft <xaisoft80@gmail.com>
2021-02-25 00:36:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <div>
-
+    {data.map(user=>(
+     <div key={user.id}>{user.name}</div>
+     )}
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {data}, // will be passed to the page component as props
+  }
 }
